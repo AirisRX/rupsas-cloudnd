7b2cd085b69a2821ad497c37fd9b56d51813d119
Airidas <airidasrupsas123@gmail.com>
2024-10-17 13:38:07 +0300

    pls good commit

--- MyWebApp/Models/Product.cs
+++ MyWebApp/Models/Product.cs
@@ -7,7 +7,7 @@ namespace MyWebApp.Models
         public int Id { get; set; }
         
         [Required]
-        public required string Name { get; set; }
+        public string? Name { get; set; }
         
         public decimal Price { get; set; }
     }
--- MyWebApp/obj/Debug/net6.0/MyWebApp.AssemblyInfo.cs
+++ MyWebApp/obj/Debug/net6.0/MyWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c534a999bd39e54e574c3967629aa7028a789cbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
