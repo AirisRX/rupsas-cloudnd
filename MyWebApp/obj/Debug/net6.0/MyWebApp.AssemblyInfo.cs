b4cc637e21957d709f251a0b33bebf80f51eb99c
Airidas <airidasrupsas123@gmail.com>
2024-11-07 12:36:27 +0200

    c

--- MyWebApp/obj/Debug/net6.0/MyWebApp.AssemblyInfo.cs
+++ MyWebApp/obj/Debug/net6.0/MyWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7972ad67fa8a2ea8e399876b427ee943179e5aff")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
